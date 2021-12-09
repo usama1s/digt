9771ba7e5e404ffc9ac9f0902a183b5778b16494
usama1s <usama_1s@hotmail.com>
2021-12-10 00:38:28 +0500

    updating

--- assets/js/main.js
+++ assets/js/main.js
@@ -342,8 +342,8 @@ $(document).ready(function() {
  interval: 60,
  // minTime: '10',
  // maxTime: '6:00pm',
- // defaultTime: '11',
- startTime: '10:00',
+ defaultTime: '09',
+ startTime: '09:00',
  dynamic: false,
  dropdown: true,
  scrollbar: true
